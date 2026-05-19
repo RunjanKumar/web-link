11c44c035a38a1f18e6d1ca492446e03182c8f4d
Runjun kumar <runjankumar786@gmail.com>
2026-05-19 18:24:27 +0530

    fix back button issues

--- src/UI/food/components/header.jsx
+++ src/UI/food/components/header.jsx
@@ -1,16 +1,18 @@
-import { ArrowLeft, MoreHorizontal } from "lucide-react";
+import { MoreHorizontal } from "lucide-react";
+import BackButton from "../../../globalComponents/BackButton";
 
 export default function Header() {
   return (
     <div>
       <div className="flex items-center justify-between mb-8">
-        <ArrowLeft className="w-7 h-7" />
+        {/* <ArrowLeft className="w-7 h-7" /> */}
+        <BackButton />
         <MoreHorizontal className="w-7 h-7" />
       </div>
 
       <h1 className="text-5xl font-bold">Food Order</h1>
 
-      <p className="text-gray-400 mt-2 text-lg">Room 208</p>
+      {/* <p className="text-gray-400 mt-2 text-lg">Room 208</p> */}
     </div>
   );
 }
